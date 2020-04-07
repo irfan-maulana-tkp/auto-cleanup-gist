12709331cd61c94875a552481471f323ff37e515
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-04-07 10:15:19 +0700

    feat: add async func

--- cleanup.js
+++ cleanup.js
@@ -4,20 +4,24 @@ const octokit = new Octokit({
   auth: process.env.GIST_TOKEN,
 });
 
-try {
-  const gists = await octokit.gists.listPublicForUser({
-    username: process.env.GIST_USERNAME,
-  });
+const runCleanup = async () => {
+	try {
+		const gists = await octokit.gists.listPublicForUser({
+			username: process.env.GIST_USERNAME,
+		});
 
-  if (gists.data && gists.data.length > 0) {
-    gists.data.map(gist => {
-      clientWithAuth.gists.delete({
-        gist_id: gist.id
-      });
-      const m = `> Delete gist file ${gist.html_url}`;
-      console.warn(m);
-    });
-  }
-} catch (e) {
-  console.error('> Error deleteAllGist: ', e)
+		if (gists.data && gists.data.length > 0) {
+			gists.data.map(gist => {
+				clientWithAuth.gists.delete({
+					gist_id: gist.id
+				});
+				const m = `> Delete gist file ${gist.html_url}`;
+				console.warn(m);
+			});
+		}
+	} catch (e) {
+		console.error('> Error deleteAllGist: ', e)
+	}
 }
+
+runCleanup();
